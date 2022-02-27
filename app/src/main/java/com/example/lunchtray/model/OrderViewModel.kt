7fb12eecd184a2ec1e7710d4b7d81fa57667f4c9
windlov2 <windlov2@gmail.com>
2022-02-27 10:59:13 +0900

    fix. remove korean comment

--- app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
+++ app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
@@ -131,7 +131,6 @@ class OrderViewModel : ViewModel() {
         // TODO: if _subtotal.value is not null, update it to reflect the price of the recently
         //  added item.
         //  Otherwise, set _subtotal.value to equal the price of the item.
-        // 첫번째 expression 과 if-else statement 가 동일한지 확인하자
 //        _subtotal.value = _subtotal.value?.plus(itemPrice) ?: itemPrice
         if (_subtotal.value != null) {
             _subtotal.value = _subtotal.value!!.plus(itemPrice)
